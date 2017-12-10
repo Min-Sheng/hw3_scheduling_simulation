2442f096b8b5b0138b28d02c7af26e64cca8307a
F94036089 <f94036089@ncku.edu.tw>
2017-12-10 18:38:20 +0800

    Make the shell command interface.

--- scheduling_simulator.c
+++ scheduling_simulator.c
@@ -22,5 +22,35 @@ int hw_task_create(char *task_name)
 
 int main()
 {
+	char command[1024];
+	int a = 1;
+	while(1){
+		char * type;
+		if(a==1){
+			printf("$ ");
+		}
+		a = scanf("%s", command);
+		if (a == 1)
+		{
+			type=strtok(command," ");
+			if(strcmp(type, "add")==0){
+				printf("added\n");
+			}else if(strcmp(type, "remove")==0){
+				printf("removed\n");
+			}else if(strcmp(type, "start")==0){
+				printf("simulating...\n");
+				break;
+			}
+			else if (strcmp(type, "ps") == 0)
+			{
+				printf("information is...\n");
+			}else{
+
+			}
+		}else{
+			fflush(stdin);
+		}
+	}
+
 	return 0;
 }
