96a31da1c778a4db486a2d27d9a745358ea78f81
F94036089 <f94036089@ncku.edu.tw>
2017-12-10 20:19:22 +0800

    Complete the shell command interface.

--- scheduling_simulator.c
+++ scheduling_simulator.c
@@ -23,29 +23,67 @@ int hw_task_create(char *task_name)
 int main()
 {
 	char command[1024];
-	int a = 1;
-	while(1) {
+	while (1)
+	{
 		char * type;
-		if(a==1) {
-			printf("$ ");
-		}
-		a = scanf("%s", command);
-		if (a == 1) {
-			type=strtok(command," ");
-			if(strcmp(type, "add")==0) {
-				printf("added\n");
-			} else if(strcmp(type, "remove")==0) {
-				printf("removed\n");
-			} else if(strcmp(type, "start")==0) {
-				printf("simulating...\n");
-				break;
-			} else if (strcmp(type, "ps") == 0) {
-				printf("information is...\n");
-			} else {
-
+		printf("$ ");
+		fgets(command,sizeof(command),stdin);
+		command[strlen(command) - 1] = 0;
+		type=strtok(command," ");
+		if (strcmp(type, "add") == 0)
+		{
+			char *task_name;
+			int time_quantum = 10;
+			char *c;
+			char *t;
+			task_name = strtok(NULL, " ");
+			c=strtok(NULL, " ");
+			t=strtok(NULL, " ");
+			if(task_name!=NULL){
+				printf("task name: %s\n", task_name);
+				if(c!=NULL){
+					if(strcmp(c,"-t")==0){
+						if(t!=NULL){
+							if(strcmp(t,"L")==0){
+								time_quantum = 20;
+							}else if (strcmp(t, "S") == 0){
+								time_quantum = 10;
+							}else{
+								printf("The correct time quantum code should be entered.\n");
+							}
+							printf("time quantum (ms): %d\n", time_quantum);
+						}else{
+							printf("Use default time quantum.\n");
+							printf("time quantum (ms): %d\n", time_quantum);
+						}
+					}else{
+						printf("Correct parameter should be topped with '-t'.\n");
+						printf("time quantum (ms): %d\n", time_quantum);
+					}
+				}else{
+					printf("time quantum (ms): %d\n", time_quantum);
+				}
+			}else{
+				printf("The task name should be entered.\n");
 			}
+		} else if(strcmp(type, "remove")==0) {
+			char *PID;
+			PID = strtok(NULL, " ");
+			if (PID != NULL)
+			{
+				pid_t pID;
+				pID = atoi(PID);
+				printf("Removed task's PID: %d\n", (int)pID);
+			}else{
+				printf("The PID should be entered.\n");
+			}
+		} else if(strcmp(type, "start")==0) {
+			printf("simulating...\n");
+			break;
+		} else if (strcmp(type, "ps") == 0) {
+			printf("information is...\n");
 		} else {
-			fflush(stdin);
+
 		}
 	}
 
