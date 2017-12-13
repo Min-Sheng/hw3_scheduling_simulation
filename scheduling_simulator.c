f2817e5b42628dce3e43db97efa3288d8d080756
F94036089 <f94036089@ncku.edu.tw>
2017-12-13 17:10:25 +0800

    Make the queue.

--- scheduling_simulator.c
+++ scheduling_simulator.c
@@ -1,5 +1,20 @@
 #include "scheduling_simulator.h"
 
+struct Data {
+	int pid;
+    char task_name[10];
+	enum TASK_STATE task_state;
+	int time_quantum;
+	int queueing_time;
+};
+struct Node
+{
+	struct Data data;
+	struct Node *next;
+};
+
+struct Node* head = NULL;
+
 void hw_suspend(int msec_10)
 {
 	return;
@@ -17,8 +32,19 @@ int hw_wakeup_taskname(char *task_name)
 
 int hw_task_create(char *task_name)
 {
+	while (current != NULL && current->data.pid != pid)
+    {
+        prev = current;
+        current = current->next;
+    }
 	return 0; // the pid of created task name
 }
+
+void add_task(char *task_name, int time_quantum);
+void remove_task(int pid);
+void start_simulation();
+void process_status();
+void free_all();
 
 int main()
 {
@@ -27,6 +53,8 @@ int main()
 		char * type;
 		printf("$ ");
 		fgets(command,sizeof(command),stdin);
+		if(strcmp(command,"\n")==0)
+			continue;
 		command[strlen(command) - 1] = 0;
 		type=strtok(command," ");
 		if (strcmp(type, "add") == 0) {
@@ -38,7 +66,7 @@ int main()
 			c=strtok(NULL, " ");
 			t=strtok(NULL, " ");
 			if(task_name!=NULL) {
-				printf("task name: %s\n", task_name);
+				//printf("task name: %s\n", task_name);
 				if(c!=NULL) {
 					if(strcmp(c,"-t")==0) {
 						if(t!=NULL) {
@@ -48,29 +76,32 @@ int main()
 								time_quantum = 10;
 							} else {
 								printf("The correct time quantum code should be entered.\n");
+								continue;
 							}
-							printf("time quantum (ms): %d\n", time_quantum);
 						} else {
 							printf("Use default time quantum.\n");
-							printf("time quantum (ms): %d\n", time_quantum);
 						}
 					} else {
 						printf("Correct parameter should be topped with '-t'.\n");
-						printf("time quantum (ms): %d\n", time_quantum);
+						continue;
 					}
 				} else {
-					printf("time quantum (ms): %d\n", time_quantum);
+					//printf("time quantum (ms): %d\n", time_quantum);
 				}
-			} else {
+				add_task(task_name, time_quantum);
+			}
+			else
+			{
 				printf("The task name should be entered.\n");
 			}
 		} else if(strcmp(type, "remove")==0) {
 			char *PID;
 			PID = strtok(NULL, " ");
 			if (PID != NULL) {
-				pid_t pID;
-				pID = atoi(PID);
-				printf("Removed task's PID: %d\n", (int)pID);
+				int pid;
+				pid = atoi(PID);
+				//printf("Removed task's PID: %d\n", pid);
+				remove_task(pid);
 			} else {
 				printf("The PID should be entered.\n");
 			}
@@ -79,10 +110,89 @@ int main()
 			break;
 		} else if (strcmp(type, "ps") == 0) {
 			printf("information is...\n");
+			process_status();
 		} else {
-
+			free_all();
 		}
 	}
-
 	return 0;
+}
+void add_task(char *task_name, int time_quantum){
+	//printf("Added task:\n");
+	//printf("task name: %s\n", task_name);
+	//printf("time quantum (ms): %d\n", time_quantum);
+	struct Node *last = head;
+	struct Node *newNode;
+	newNode = malloc(sizeof(struct Node));
+	int pid = hw_task_create(task_name);
+	strcpy(newNode->data.task_name, task_name);
+	newNode->data.pid=pid;
+	newNode->data.task_state=TASK_READY;
+	newNode->data.time_quantum=time_quantum;
+	newNode->next = NULL;
+    if (head == NULL)
+    {
+		head = newNode;
+		return;
+	}
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+    last->next = newNode;
+    return;
+}
+void remove_task(int pid){
+	struct Node *current = head;
+	struct Node *prev;
+	// If head node itself holds the pid to be deleted
+    if (current != NULL && current->data.pid == pid)
+    {
+        head = current->next;
+        free(current);
+        return;
+    }
+
+    // Search for the pid to be deleted, keep track of the
+    // previous node as we need to change 'prev->next'
+    while (current != NULL && current->data.pid != pid)
+    {
+        prev = current;
+        current = current->next;
+    }
+
+    // If pid was not present in linked list
+    if (current == NULL){
+		printf("No such pid in the queue.\n");
+		return;
+	}
+
+    // Unlink the node from linked list
+    prev->next = current->next;
+
+    free(current);
+	return;
+}
+void process_status(){
+	struct Node *current = head;
+	if(current==NULL){
+		printf("No task in the queue.\n");
+		return;
+	}
+	while(current != NULL){
+		printf("%d\t%s\t%d\t%d\n", current->data.pid, current->data.task_name,current->data.task_state, current->data.time_quantum);
+        current = current->next;
+    }
+}
+
+void free_all(){
+	struct Node* current = head;
+   	struct Node* next;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	head = NULL;
 }
